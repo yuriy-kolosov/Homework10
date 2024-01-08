d68eed66e8ee71d617a62fafc45a149b3ceb5bde
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-01-08 22:44:05 +0300

    Домашнее задание 10 - задачи 1...3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание 10");
+        System.out.println();
+//
+        System.out.println("          Задача 10-1");
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
+//
+        System.out.println("          Задача 10-2");
+        firstName = "Иван";
+        middleName = "Иванович";
+        lastName = "Иванов";
+        fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Данные Ф.И.О. сотрудника для отчета - " + fullName.toUpperCase());
+        System.out.println();
+//
+        System.out.println("          Задача 10-3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Сотрудник - " + fullName);
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName.replace("ё", "е"));
+        System.out.println();
     }
 }
